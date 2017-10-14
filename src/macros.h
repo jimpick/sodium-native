670220e62e3b09d50a6f919fe5bf5405b99ed637
Emil Bay <github@tixz.dk>
2017-10-14 17:14:57 +0200

    Check IsUint8Array instead of IsObject

--- src/macros.h
+++ src/macros.h
@@ -55,7 +55,7 @@
   info.GetReturnValue().Set(ret == 0 ? Nan::True() : Nan::False());
 
 #define ASSERT_BUFFER(name, var) \
-  if (!name->IsObject()) { \
+  if (!name->IsUint8Array()) { \
     Nan::ThrowError(#var " must be a buffer"); \
     return; \
   } \
